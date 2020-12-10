68d245e6bd47f349396dea17d1c13589a4346efa
Patrice Cottman <patrice.cottman@gmail.com>
2020-12-10 20:21:40 +0000

    movies are displaying, added style sheet

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import DisplayMovies from './DisplayMovies.jsx';
+
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 const App = (props) => (
-  <div>Hello!</div>
+  <div>
+    <DisplayMovies movies = {movies}/>
+  </div>
 );
 
 export default App;
---
+++ client/src/components/DisplayMovies.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+const DisplayMovies = (props) => (
+
+  <div className = 'moviebox'>
+
+   {props.movies.map(({title}, idx) => {
+
+
+      return (
+
+        <span  key = {title + idx} value = {idx}>{title}</span>
+
+      )
+
+
+   })}
+    </div>
+
+);
+
+
+export default DisplayMovies;
